d60c44e343cb9c64234d9a6a765b70080b62c0a8
Bernhard Berger <berger_bernhard@gmx.net>
2019-03-05 20:14:58 +0100

    The player position indicator can now move to the northernmost row when moving North East.

--- src/com/rule2d/main/KeyboardControl.java
+++ src/com/rule2d/main/KeyboardControl.java
@@ -575,7 +575,7 @@ public class KeyboardControl {
 				///// Move forward while facing North West /////
 				//////////////////////////////////////////////////
 				case "NW":
-					if (Rule2D.intPlayerLatitude - 1 <= 0) {
+					if (Rule2D.intPlayerLatitude - 1 < 0) {
 						// Walking over the edge of the map, we arrive at the other side of the globe via the pole.
 						if (Rule2D.intPlayerLongitude - Rule2D.MAPWIDTH/2 < 0) {
 							Rule2D.intPlayerLongitude = Rule2D.MAPWIDTH - (Rule2D.MAPWIDTH/2 - Rule2D.intPlayerLongitude);							
